264bd52e83b4bb905f1c913a8b1e0033d55bcec8
Ajarnnotes <ajarnnotes@gmail.com>
2020-12-16 23:51:11 +0700

    EDIT home page css

--- playlist-frontend/src/pages/Home.js
+++ playlist-frontend/src/pages/Home.js
@@ -5,8 +5,6 @@ import Content from "../components/Content";
 
 const Container = styled.div`
   width: 100vw;
-  height: 100vh;
-  background-color: blue;
   margin: 0;
   padding: 0;
   display: flex;
